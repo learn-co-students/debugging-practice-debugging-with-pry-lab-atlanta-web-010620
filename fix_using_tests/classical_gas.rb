f3638a27daf15a12f76dcf441c34b773ba99adcd
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-17 02:03:55 +0000

    Done.

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,6 +1,8 @@
+require 'pry'
+
 class GasStation
 
-  attr_reader :brand, :unleaded_price
+  attr_accessor :brand, :unleaded_price
 
   def initialize(brand, unleaded_price)
     @brand = brand
@@ -12,3 +14,5 @@ end
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+
+
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+    10.times {string = "s" + string}
   else
   string
   end
+  return string
 end
